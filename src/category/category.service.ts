5296953847ff1024a91a351c2f9b05908f50e025
Khang China <khang.nguyen@htgsoft.com>
2024-08-21 16:56:08 +0700

    create migration

--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -4,13 +4,21 @@ import { UpdateCategoryInput } from './dto/update-category.input';
 import { Category } from './entities/category.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { faker } from '@faker-js/faker';
 
 @Injectable()
 export class CategoryService {
-  constructor(@InjectRepository(Category) private category: Repository<Category>) {}
+  constructor(@InjectRepository(Category) private category: Repository<Category>) { }
   async create(createCategoryInput: CreateCategoryInput) {
     const cate = await this.category.create(createCategoryInput);
     return await this.category.save(cate);
+  }
+  async migration_data_test() {
+    for (let index = 0; index < 5; index++) {
+      const createCategoryInput: CreateCategoryInput = {
+        name : faker.commerce.department()
+      }
+    }
   }
 
   findAll() {
